7d4f8cc0e37ba0d1cf46f7da1886f6a6d0aa32a2
Nate <foxyfan23@gmail.com>
2024-07-05 21:52:51 -0500

    third todo done

--- coffee-shop/src/pages/api/cart/index.js
+++ coffee-shop/src/pages/api/cart/index.js
@@ -5,9 +5,13 @@ export default function handler(req, res) {
     const cartItems = db.cart.get();
     res.status(200).json({ cart: cartItems });
   } else if (req.method === "POST") {
-    // TODO #1 use the db.cart.getById method to see if we already have the item in the cart
-    // IF we do have the item in the cart already, then update the quantity, otherwise add the item
-    db.cart.add(req.body);
+    const menuId = req.body.id;
+    const itemInCartAlready = db.cart.getById(menuId);
+    if (itemInCartAlready) {
+      db.cart.updateById(menuId, itemInCartAlready.quantity + 1);
+    } else {
+      db.cart.add(req.body);
+    }
     res.status(200).json({ message: "Item added to cart" });
   } else {
     res.status(404).json({ message: "We only support GET requests" });
